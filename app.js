f5f167918ac5b715179cabe90197ffa6e5ef3eaf
ABHISHEK KUMAR <ak5703@srmist.edu.in>
2023-09-29 17:45:30 +0530

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-const port= process.env.PORT || 5000;
+const PORT= process.env.PORT || 5000;
 const mongoose=require('mongoose');
 const {mongoUrl}=require('./keys')
 const cors=require('cors')
@@ -27,6 +27,6 @@ mongoose.connection.on("error",()=>{
 })
 
 
-app.listen(port,()=>{
-    console.log("Server running on port "+port)
+app.listen(PORT,()=>{
+    console.log("Server running on port "+PORT)
 })
